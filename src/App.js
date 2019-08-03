5e160c220e46977b3a55d1cf1c3182d796090bb6
Chad Jemmett <chad.jemmett@gmail.com>
2019-08-03 14:50:43 -0600

    Workgin on list

--- src/App.js
+++ src/App.js
@@ -2,25 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const handleChange = () => {
 }
+
+
+class App extends React.Component {
+    constructor() {
+      super()
+
+      this.state = {
+      list: [],
+      status: {total: 0, done: 0},
+      newItem: "",
+      idNumber: 0,
+     }
+  }
+
+  handleChange = (e) => {
+    this.setState({...this.state, newItem: e.target.value})
+  }
+
+  newItem = (e) => {
+    // e.preventDefault()
+    let newNumber = this.state.idNumber += 1
+    this.setState({list: [...this.state.list, {itemName: this.state.newItem, id: newNumber, completed: false}]})
+    console.log(this.state.list)
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Todo list</h1>
+        <input onChange={this.handleChange}>
+        </input>
+        <button onClick={this.newItem}>Add</button>
+      </div>
+    );
+  }
+
+}
+
 
 export default App;
